4588a7b1d072650ee6c4ebac8b2eb44a8b79464c
Joseph Cox <joeyhobart@outlook.com>
2023-07-29 20:25:29 -0400

    Cleanup codes, use mosaic

--- dist/scripts/frontend/home.js
+++ dist/scripts/frontend/home.js
@@ -114,160 +114,61 @@ async function loadCodes() {
     });
     const codesData = await codesResponse.json();
 
-    if (codesData.status === "ok") {
-        codesList.innerHTML = "";
-        if (codesData.data.length === 0) {
-            const noCodes = document.createElement("p");
-            noCodes.innerText = "No codes yet! Try creating one :)";
-            codesList.appendChild(noCodes);
-        } else {
-            const headerRow = document.createElement("tr");
-            headerRow.id = "headerRow";
-            headerRow.insertCell(0).outerHTML = "<th>Name</th>";
-            headerRow.insertCell(1).outerHTML = "<th>Visits</th>";
-            headerRow.insertCell(2).outerHTML = "<th>Actions</th>";
-            headerRow.insertCell(3).outerHTML = "<th>QR Code</th>";
-            codesList.appendChild(headerRow);
+    if (codesData.status == "ok") {
+        generateCodes(codesData.data);
+    }
+}
 
-            for (const code of codesData.data) {
-                const tableRow = document.createElement("tr");
-                tableRow.id = code._id;
-                tableRow.className = "codeRow";
+function generateCodes(codes) {
+    const codesList = document.getElementById("codesList");
+    codesList.innerHTML = "";
 
-                // Create table data elements
-                const nameTd = document.createElement("td");
-                nameTd.classList.add("name");
-                nameTd.innerHTML = `<a href="${code.redirect_url}"> ${code.name}</a>`;
+    var maxColumns = 4; // Set this to whatever maximum number of columns you want
+    var columns = Math.min(codes.length, maxColumns);
+    codesList.style.maxWidth = `calc((180px + 10px) * ${columns})`;
 
-                const currentBaseURL = window.location.href.split("/")[2];
-                const visitsTd = document.createElement("td");
-                visitsTd.classList.add("visits");
-                visitsTd.innerHTML = `<a href='/explore/${code.short_id}'>${code.visits}</a>`;
+    for (const code of codes) {
+        const codeDiv = document.createElement("div");
+        codeDiv.classList.add("code", "roundedBox");
+        codeDiv.id = code._id;
 
-                const buttonsTd = document.createElement("td");
-                buttonsTd.classList.add("codeButtons");
+        const name = document.createElement("h3");
+        name.innerText = code.name;
+        name.classList.add("codeName");
 
-                const deleteButton = document.createElement("button");
-                deleteButton.classList.add("btn", "btn-danger", "deleteButton");
-                deleteButton.innerText = "Delete";
-                deleteButton.addEventListener("click", async (event) => {
-                    if (
-                        !confirm("Are you sure you want to delete this code?")
-                    ) {
-                        return;
-                    }
-                    event.preventDefault();
-                    const response = await fetch("/api/post/deletecode/", {
-                        method: "POST",
-                        headers: {
-                            "Content-Type": "application/json",
-                        },
-                        body: JSON.stringify({
-                            id: code._id,
-                        }),
-                    });
-                    const data = await response.json();
-                    if (data.status === "ok") {
-                        document.getElementById(code._id).remove();
-                    }
-                });
-                buttonsTd.appendChild(deleteButton);
+        const visits = document.createElement("p");
+        visits.innerText = `Scans: ${code.visits}`;
+        visits.classList.add("codeVisits");
 
-                const testLinkButton = document.createElement("button");
-                testLinkButton.classList.add(
-                    "btn",
-                    "btn-primary",
-                    "testButton"
-                );
-                testLinkButton.innerText = "Copy Link";
-                testLinkButton.addEventListener("click", async (event) => {
-                    event.preventDefault();
-                    let url;
-                    if (code.type == "qr") {
-                        url = `${currentBaseURL}/code?id=${code._id}`;
-                    } else {
-                        url = url = `${currentBaseURL}/link/${code.short_id}`;
-                    }
-                    navigator.clipboard.writeText(url);
-                    testLinkButton.innerText = "Copied!";
-                    setTimeout(() => {
-                        testLinkButton.innerText = "Copy Link";
-                    }, 2000);
-                });
-                buttonsTd.appendChild(testLinkButton);
+        const buttons = document.createElement("div");
+        buttons.classList.add("codeButtons");
 
-                if (USER_DATA.plan > 0 && code.isDynamic) {
-                    const updateLinkButton = document.createElement("button");
-                    updateLinkButton.classList.add(
-                        "btn",
-                        "btn-primary",
-                        "updateButton"
-                    );
-                    updateLinkButton.innerText = "Update link";
-                    updateLinkButton.dataset.id = code._id;
-                    updateLinkButton.addEventListener(
-                        "click",
-                        async (event) => {
-                            event.preventDefault();
-                            const newURL = prompt("Enter a new URL:");
-                            if (newURL === null) {
-                                return;
-                            }
-                            const response = await fetch(
-                                "/api/post/updatecode/",
-                                {
-                                    method: "POST",
-                                    headers: {
-                                        "Content-Type": "application/json",
-                                    },
-                                    body: JSON.stringify({
-                                        id: updateLinkButton.dataset.id,
-                                        newURL: newURL,
-                                    }),
-                                }
-                            );
-                            const data = await response.json();
-                            if (data.status === "ok") {
-                                window.location.reload();
-                            }
-                        }
-                    );
-                    buttonsTd.appendChild(updateLinkButton);
-                }
+        const deleteButton = document.createElement("button");
+        deleteButton.classList.add("btn", "btn-danger", "deleteButton");
+        deleteButton.innerText = "Delete";
+        deleteButton.addEventListener("click", async (event) => {});
+        buttons.appendChild(deleteButton);
 
-                // Append created table data elements to the row
-                tableRow.append(nameTd, visitsTd, buttonsTd);
-                const qrCodeTd = document.createElement("td");
-                qrCodeTd.classList.add("qrCode");
-                if (code.type === "qr") {
-                    const img = document.createElement("img");
-                    img.src = code.code;
-                    img.classList.add("card-img-top");
-                    const pngImg = await fetch(code.code);
-                    const pngBlob = await pngImg.blob();
-                    const qrCodeImageFile = new File([pngBlob], "qr.png", {
-                        type: "image/png",
-                    });
-                    img.addEventListener("click", async () => {
-                        const url = window.URL.createObjectURL(qrCodeImageFile);
-                        const a = document.createElement("a");
-                        a.href = url;
-                        a.download = "qualitycodesQR.png";
-                        document.body.appendChild(a);
-                        a.click();
-                        a.remove();
-                        window.URL.revokeObjectURL(url);
-                    });
-                    qrCodeTd.appendChild(img);
-                } else {
-                    qrCodeTd.innerHTML =
-                        "<span style='color: gray;'>N/A</span>";
-                }
-                tableRow.appendChild(qrCodeTd);
+        const copyLinkButton = document.createElement("button");
+        copyLinkButton.classList.add("btn", "copyButton");
+        copyLinkButton.innerText = "Copy Link";
+        copyLinkButton.addEventListener("click", async (event) => {});
+        buttons.appendChild(copyLinkButton);
 
-                codesList.appendChild(tableRow);
-            }
+        codeDiv.appendChild(name);
+        codeDiv.appendChild(visits);
+        codeDiv.appendChild(buttons);
+
+        if (code.type === "qr") {
+            const QRcode = document.createElement("img");
+            QRcode.src = code.code;
+            QRcode.classList.add("card-img-top");
+            QRcode.addEventListener("click", async () => {});
+            QRcode.classList.add("card-img-top");
+            codeDiv.appendChild(QRcode);
         }
+
+        codesList.appendChild(codeDiv);
     }
 }
 
